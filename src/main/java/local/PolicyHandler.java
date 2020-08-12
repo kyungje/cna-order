2d9994bfd4dfb9752f2a7daedd1c2ac87712459f
cnynaver <cnyna@naver.com>
2020-08-12 13:49:15 +0900

    first commit

---
+++ src/main/java/local/PolicyHandler.java
@@ -0,0 +1,35 @@
+package local;
+
+import local.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PolicyHandler{
+    @Autowired
+    OrderRepository orderRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onOrderPlaced(@Payload Shipped shipped) {
+        System.out.println("shipped.getOrderId(): " + shipped.getOrderId());
+        if(shipped.isMe()) {
+            // 재고량 수정
+            Optional<Order> orderOptional = orderRepository.findById(shipped.getOrderId());
+            Order order = orderOptional.get();
+
+            orderRepository.save(order);
+        }
+    }
+
+}
